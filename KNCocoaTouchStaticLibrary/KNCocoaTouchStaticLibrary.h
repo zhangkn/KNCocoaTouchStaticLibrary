2c5d467dcb436d6508469ab299729976e129ed21
kevin <929118967@qq.com>
2017-06-29 19:17:29 +0800

    Initial Commit

---
+++ KNCocoaTouchStaticLibrary/KNCocoaTouchStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  KNCocoaTouchStaticLibrary.h
+//  KNCocoaTouchStaticLibrary
+//
+//  Created by devzkn on 29/06/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNCocoaTouchStaticLibrary : NSObject
+
+@end
---
+++ KNCocoaTouchStaticLibrary/KNCocoaTouchStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  KNCocoaTouchStaticLibrary.m
+//  KNCocoaTouchStaticLibrary
+//
+//  Created by devzkn on 29/06/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import "KNCocoaTouchStaticLibrary.h"
+
+@implementation KNCocoaTouchStaticLibrary
+
+@end
